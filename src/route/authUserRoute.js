dc2157bdbb1c4f79041fb4f9d29af938cb9ff8ea
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-28 16:41:53 -0300

    feat: fix nethod put, now works well

--- src/route/authUserRoute.js
+++ src/route/authUserRoute.js
@@ -3,7 +3,6 @@ import UsersModel from "../model/userModel.js";
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
-
 const userRoute = Router();
 
 userRoute.get("/users/:id", async (req, res) => {
@@ -14,14 +13,14 @@ userRoute.get("/users/:id", async (req, res) => {
   }
 
   try {
-    const users = await UsersModel.find({ id });
+    const users = await UsersModel.find({ _id: id });
     res.send({ users });
   } catch (error) {
     res.status(400).send({ error: "Failed to find user" });
   }
 });
 
-userRoute.post("/users", async (req, res, next) => {
+userRoute.post("/users", async (req, res) => {
   const { username, password, email } = req.body;
 
   if (!username || !password || !email) {
@@ -44,26 +43,30 @@ userRoute.post("/users", async (req, res, next) => {
 
     res.status(201).send({ user });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({ erro: "it email already register" });
   }
 });
 
-userRoute.put("/users/:id", async (req, res, next) => {
+userRoute.put("/users/:id", async (req, res) => {
   const { id } = req.params;
 
   if (!id) {
     return res.status(400).send({ error: "missing params id" });
   }
   try {
-    const updateUser = new UsersModel.findOneAndUpdate({ id }, req.body, {
-      new: true,
-    });
+    const updateUser = await UsersModel.findOneAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        new: true,
+      }
+    );
 
     if (!updateUser) {
       res.status(400).send({ error: "Could not update the user" });
     }
 
-    req.status(200).send(updateUser);
+    res.status(201).send({ user: updateUser });
   } catch (error) {
     res.send(error);
   }
